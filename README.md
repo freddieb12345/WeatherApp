d4dc6b6b0dd1a5e3b14a8a6a833ba004c16922ee
Freddie Brewin <freddiebrewin@live.com>
2021-03-28 22:07:14 +0100

    Added comments and readme file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # WeatherApp
+This weather app allows you to search for any city and get a weather forecast for that city for the current day and the next 5 days.
+Live URL:   https://freddieb12345.github.io/WeatherApp/.
+Github Repo: https://github.com/freddieb12345/WeatherApp
+Screenshot: ![image](https://user-images.githubusercontent.com/58634971/112768036-de19ab00-9011-11eb-978c-ec078d435949.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-const userInputEl = document.getElementById("city-input");
+//Getting elements by their ID
+const userInputEl = document.getElementById("user-input");
 const searchButtonEl = document.getElementById("search-button");
 const cityNameEl = document.getElementById("city-name");
 const tempEl = document.getElementById("temperature");
@@ -7,13 +8,14 @@ const windEl = document.getElementById("wind-speed");
 const uvEl = document.getElementById("UV-index");
 const searchHistoryEl = document.getElementById("history");
 const clearHistoryBtn = document.getElementById("clear-history")
-const imgEl = document.getElementById("current-pic");
+const imgEl = document.getElementById("current-img");
+
 
 const APIKey = "73e080bd08077adb9fa1fd1d913233fc";
-var searchHistory = JSON.parse(localStorage.getItem("input")) || [];
+var searchHistory = JSON.parse(localStorage.getItem("input")) || []; //Defining search history as a the input value in localStorage, or failing that, and empty array.
 
+//Function that executes the search for the cities weather
 function searchCity(cityName) {
-    /* console.log("hi"); */
     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
     
     fetch(requestUrl)
@@ -24,7 +26,6 @@ function searchCity(cityName) {
             //Display city name and date
             console.log(data);
             const currentDate = new Date(data.dt*1000);
-            /* console.log(currentDate); */
             const day = currentDate.getDate();
             const month = currentDate.getMonth() + 1;
             const year = currentDate.getFullYear();
@@ -33,7 +34,6 @@ function searchCity(cityName) {
             var weatherImg = data.weather[0].icon;
             imgEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherImg + "@2x.png");
             imgEl.setAttribute("alt", data.weather[0].description);
-
             //Display temperature in celsius
             var tempCelsius = Math.round(data.main.temp - 273); 
             tempEl.textContent = 'Temperature - ' + tempCelsius + '°C';
@@ -46,21 +46,20 @@ function searchCity(cityName) {
             //Create variables for latitude and longitude to use in the UV API
             var latitude = data.coord.lat;
             var longitude = data.coord.lon;
-
-            /* console.log(latitude);
-            console.log(longitude); */
-            /* searchUVIndex(latitude,longitude); */
+    //Fetching seperate API for the UV index
     fetch("https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey)
         .then(function(response) {
             return response.json();
         })
         .then(function(data){
             console.log(data);
+            //Creating UV index element
             uvEl.textContent = 'UV Index - ';
             var UVIndex = document.createElement("span");
             currentUVindex = data[0].value;
             UVIndex.innerHTML = currentUVindex;
             uvEl.append(UVIndex);
+            //Formatting UV index element depending on its severity
             if(currentUVindex < 2){
                 UVIndex.setAttribute("class","badge badge-success")
             }
@@ -69,16 +68,18 @@ function searchCity(cityName) {
             }
             else{
                 UVIndex.setAttribute("class","badge badge-warning")
-            }   
+            }
+    //Fetching seperate API for the 5 day forecast   
     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey)
         .then(function(response){
             return response.json();
         })
         .then(function(data){
             console.log(data);
-            var forecastElements = document.querySelectorAll(".forecast");
-            for(var i =0; i < forecastElements.length ; i++){
-                forecastElements[i].innerHTML = "";
+            var forecastElements = document.querySelectorAll(".forecast"); //Selectss all elements with the class 'forecast'
+            for(var i =0; i < forecastElements.length ; i++){ //Creates elements equal to the number of elements selected above
+                forecastElements[i].innerHTML = ""; //Clears the elements initially
+                //Gets date of forecast and appends it to elements
                 const forecastIndex = i*8 + 4; //Selects 9am every morning to take the forecast
                 const forecastDate = new Date(data.list[forecastIndex].dt*1000);
                 const forecastDay = forecastDate.getDate();
@@ -88,17 +89,17 @@ function searchCity(cityName) {
                 forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
                 forecastDateEl.textContent = forecastDay + '/' + forecastMonth + '/' + forecastYear;
                 forecastElements[i].append(forecastDateEl);
-
+                //Gets corresponding img for the weather and appends it to the elements
                 const forecastImgEl = document.createElement("img");
                 forecastImgEl.setAttribute("src","https://openweathermap.org/img/wn/" + data.list[forecastIndex].weather[0].icon + "@2x.png");
                 forecastImgEl.setAttribute("alt", data.list[forecastIndex].weather[0].description);
                 forecastElements[i].append(forecastImgEl);
-
+                //Gets forecast temps and appends them to the elements
                 const forecastTempEl = document.createElement("p");
                 const tempCelsiusI = Math.round(data.list[forecastIndex].main.temp - 273)
                 forecastTempEl.textContent = "Temp: " + tempCelsiusI + '°C';
                 forecastElements[i].append(forecastTempEl);
-
+                //Gets forecast Humdity and appends them to the elements
                 const forecastHumidityEl = document.createElement("p");
                 forecastHumidityEl.textContent = "Humidity: " + data.list[forecastIndex].main.humidity + "%";
                 forecastElements[i].append(forecastHumidityEl);
@@ -108,8 +109,10 @@ function searchCity(cityName) {
     })
 }
 
+//Function to load the search history to the page 
 function loadSearchHistory() {
-    searchHistoryEl.innerHTML = "";
+    searchHistoryEl.innerHTML = ""; //Clears the search history element initially
+    //loops through the searchhistory element and prints each result to the element
     for (var i = 0; i < searchHistory.length; i++) {
         const historyResult = document.createElement("input");
         historyResult.setAttribute("type", "text");
@@ -117,15 +120,17 @@ function loadSearchHistory() {
         historyResult.setAttribute("class","form-control d-block bg-white");
         historyResult.setAttribute("value", searchHistory[i]);
         
+        //Makes search history clickable to quickly search for cities that have previously been searched
         historyResult.addEventListener("click",function() {
             searchCity(historyResult.value);
         })
-        searchHistoryEl.append(historyResult);
+        searchHistoryEl.append(historyResult); //Appends the history results to the search history element
     }
 }
 
+//Creates function that activates when search button is clicked
 searchButtonEl.addEventListener("click",function(){
-    const userInput = userInputEl.value;
+    const userInput = userInputEl.value; 
     /* console.log(userInput); */
     searchCity(userInput);
     searchHistory.push(userInput);
@@ -134,11 +139,13 @@ searchButtonEl.addEventListener("click",function(){
     loadSearchHistory();
 });
 
+//Creates clear history button functionallity
 clearHistoryBtn.addEventListener("click", function() {
     searchHistory = [];
     loadSearchHistory();
 })
 
+//Loads last search city on page load
 loadSearchHistory();
 if(searchHistory.length > 0) {
     searchCity(searchHistory[searchHistory.length -1]);
