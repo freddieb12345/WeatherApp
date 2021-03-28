3ace5a9a4e6beab99b657e4d6845411b92577ddf
Freddie Brewin <freddiebrewin@live.com>
2021-03-28 14:07:06 +0100

    Fetched data from open weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 const userInputEl = document.getElementById("city-input");
 const searchButtonEl = document.getElementById("search-button");
+const cityNameEl = document.getElementById("city-name");
+
 const APIKey = "73e080bd08077adb9fa1fd1d913233fc";
 var searchHistory = JSON.parse(localStorage.getItem("input")) || [];
 
-function searchCity() {
+function searchCity(cityName) {
     console.log("hi");
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    fetch(requestUrl)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            cityNameEl.textContent = data.name;
+        })
 }
 
 searchButtonEl.addEventListener("click",function(){
